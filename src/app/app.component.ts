934e4704305c01639050992780541c0a299b6120
Cristian Torres <desancheztorres@gmail.com>
2019-03-24 12:33:24 +0000

    Authentication and logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  isLoggedIn$: Observable<boolean>;
+
+  isLoggedOut$: Observable<boolean>;
+
+  pictureUrl$: Observable<string>;
+
+  constructor(private afAuth: AngularFireAuth) {
+
+  }
+
+  ngOnInit() {
+    this.afAuth.authState.subscribe( user => console.log(user));
+
+    // !! To transform the user variable to boolean
+    this.isLoggedIn$ = this.afAuth.authState
+      .pipe(
+        map( user => !!user)
+      );
+    
+      this.isLoggedOut$ = this.isLoggedIn$
+        .pipe(
+          map(loggedIn => !loggedIn)
+        );
+
+      this.pictureUrl$ = this.afAuth.authState
+          .pipe(
+            map(user => user ? user.photoURL : null)
+          )
+  }
 
 
     logout() {
-
+      this.afAuth.auth.signOut();
     }
 
 }
